90156bb3b20a8f264b41aefd1efefd17bf7a67f6
Weslley Gatti <weslleygatti1@hotmail.com>
2026-03-31 18:32:19 -0300

    Adicionado packages, getters e setters.

--- src/Principal.java
+++ src/Principal.java
@@ -1,17 +1,17 @@
+import br.com.alura.screenmatch.modelo.Filme;
+
 public class Principal {
     public static void main(String[] args) {
         Filme filme = new Filme();
-        filme.nome = "O poderoso chefão";
-        filme.anoDeLancamento = 1978;
-        filme.duracaoEmMinutos = 180;
+        filme.setNome("O poderoso chefão");
+        filme.setAnoDeLancamento(1978);
+        filme.setDuracaoEmMinutos(180);
         
         filme.showTechnical();
         filme.rate(8);
         filme.rate(5);
         filme.rate(10);
-        System.out.println(filme.totalDeAvaliacoes);
-        System.out.println(filme.somaDasAvaliacoes);
-
+        System.out.println("Total de avalições: "+ filme.getTotalDeAvaliacoes());
         System.out.println(filme.getMedia());
 
     }
