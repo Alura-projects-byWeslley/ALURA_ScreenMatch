2545339bfcde3755fc753a0f19a53178cf5b44ab
Weslley Gatti <weslleygatti1@hotmail.com>
2026-06-08 00:40:06 -0300

    Adicionado possibilidade de escolha na busca de filmes

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -1,5 +1,6 @@
 package br.com.alura.screenmatch.principal;
 
+import java.util.Scanner;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -9,9 +10,15 @@ import java.net.http.HttpResponse.BodyHandlers;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite um filme para busca: ");
+        var busca = scan.nextLine();
+
+        String url = "https://www.omdbapi.com/?t=" + busca + "&apikey=4ef38559";
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-         .uri(URI.create("https://www.omdbapi.com/?t=matrix&apikey=4ef38559"))
+         .uri(URI.create(url))
          .build();
         
         HttpResponse<String> response = client
