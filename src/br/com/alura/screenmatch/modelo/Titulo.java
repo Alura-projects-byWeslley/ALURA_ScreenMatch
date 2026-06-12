d60276cbc4276411c64ece74d4c3b0dfd8a135ff
Weslley Gatti <weslleygatti1@hotmail.com>
2026-06-12 01:37:13 -0300

    Add: sligthly mod in api receive

--- src/br/com/alura/screenmatch/modelo/Titulo.java
+++ src/br/com/alura/screenmatch/modelo/Titulo.java
@@ -1,6 +1,11 @@
 package br.com.alura.screenmatch.modelo;
+
+import com.google.gson.annotations.SerializedName;
+
 public class Titulo implements Comparable<Titulo>{
+    @SerializedName("Title")
     private String nome;
+    @SerializedName("Year")
     private int anoDeLancamento;
     private boolean incluidoNoPlano;
     private double somaDasAvaliacoes;
@@ -71,5 +76,10 @@ public class Titulo implements Comparable<Titulo>{
 
     public int compareTo(Titulo outroTitulo){
         return this.getNome().compareTo(outroTitulo.getNome());
+    }
+
+    public String toString(){
+        return "nome= '"+this.nome+ '\''+
+                ", anoDeLancamento="+anoDeLancamento;
     }
 }
